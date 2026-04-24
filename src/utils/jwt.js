eff917c17ff38fd476918171ec5b29b84b5828f9
Danish8152 <mohddanish8152@gmail.com>
2026-04-24 18:05:39 +0530

    payment error ev

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function signToken(payload) {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
 }
 
 function verifyToken(token) {
